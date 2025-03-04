00769e2701cb77a7d319c17a398ac40ec1ae0c1c
marcvanheerden <11750833+marcvanheerden@users.noreply.github.com>
2025-03-04 21:50:27 +0200

    fix number in update guide, remove breaking fzf config

--- nvim/full_update.md
+++ nvim/full_update.md
@@ -4,8 +4,8 @@
 
 1. Pull latest master branch 
 2. run `make distclean` to clear out old build cruft
-2. Check dependencies are installed 
-3. Build from source 
+3. Check dependencies are installed 
+4. Build from source 
 
 ## Lazy
 
--- nvim/lua/mvh/fzf.lua
+++ nvim/lua/mvh/fzf.lua
@@ -7,16 +7,3 @@ vim.env.PATH = fzf_path .. ":" .. vim.env.PATH
 -- hotkey for file search fzf
 vim.api.nvim_set_keymap("n", "<Leader>f", ":FZF<CR>", { noremap = true, silent = true })
 vim.fn.setenv("FZF_DEFAULT_COMMAND", "rg --files --hidden")
-
--- helper function for searching buffers with fzf
-vim.cmd([[
-  command! Buffers
-        \ call fzf#vim#buffers({
-        \   'options': [
-        \     '--preview', 
-        \     'echo {} | cut -d '' '' -f 3- | xargs bat --style=numbers --color=always --line-range :500',
-        \   ],
-        \   'down': '40%',
-        \   'header': 'Open Buffers'
-        \ })
-]])
